2b258f7904e0cebdfc3c7a277ca4a4436491716e
Chaithra-1408 <mc1104@srmist.edu.in>
2026-02-27 16:10:18 +0530

    UC2:Hardcoded palindrome

--- PracticeApp/src/PalindromeCheckerApp.java
+++ PracticeApp/src/PalindromeCheckerApp.java
@@ -1,5 +1,13 @@
 public class PalindromeCheckerApp {
-    public static void main(String args){
-        System.out.println("Welcome");
+
+    public static void main(String[] args) {
+        String word = "madam";
+        String original = word.toLowerCase();
+        String reversed = new StringBuilder(original).reverse().toString();
+        if (original.equals(reversed)) {
+            System.out.println("The word \"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a palindrome.");
+        }
     }
 }
