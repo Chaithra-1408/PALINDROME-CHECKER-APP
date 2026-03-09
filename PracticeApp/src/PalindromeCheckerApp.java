4045764860f92e30f92cf3831afdc8960928d071
Chaithra-1408 <mc1104@srmist.edu.in>
2026-03-09 15:47:33 +0530

    Added UC4 Character array based palindrome check

--- PracticeApp/src/PalindromeCheckerApp.java
+++ PracticeApp/src/PalindromeCheckerApp.java
@@ -1,5 +1,33 @@
 public class PalindromeCheckerApp {
-    public static void main(String args){
-        System.out.println("Welcome");
+    public static void main(String args []){
+        // Hardcoded string to check
+        String str = "Madam";
+
+        // Convert string to lowercase to make check case-insensitive
+        str = str.toLowerCase();
+
+        // Convert string to character array
+        char[] chars = str.toCharArray();
+
+        // Two-pointer approach
+        int left = 0;
+        int right = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                isPalindrome = false;
+                break; // Stop checking once mismatch is found
+            }
+            left++;
+            right--;
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
     }
 }
