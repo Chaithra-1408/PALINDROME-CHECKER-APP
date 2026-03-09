5d17a1efee7bc5a08cd2d61af0e630bbb32523cd
Chaithra-1408 <mc1104@srmist.edu.in>
2026-03-09 15:23:15 +0530

    Added UC2 checked String palindrome using loop in half of the string

--- PracticeApp/src/PalindromeCheckerApp.java
+++ PracticeApp/src/PalindromeCheckerApp.java
@@ -1,5 +1,23 @@
 public class PalindromeCheckerApp {
-    public static void main(String args){
-        System.out.println("Welcome");
+    public static void main(String args[]){
+        // Hardcoded string to check
+        String str = "madam";
+        boolean isPalindrome = true;
+
+        // Loop only half of the string
+        for (int i = 0; i < str.length() / 2; i++) {
+            // Compare characters from start and end
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPalindrome = false;
+                break; // No need to check further
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
     }
 }
